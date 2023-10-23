07f11640ad1ea6fd72884f482d9b3734c68749eb
doblem2000 <m.marsico10@studenti.unisa.it>
2023-10-23 18:49:17 +0200

    WP4 funzionante

--- WP4/sistema_di_stabilizzazione_meccanico_linear_controller.m
+++ WP4/sistema_di_stabilizzazione_meccanico_linear_controller.m
@@ -5,11 +5,11 @@ function ueq = sistema_di_stabilizzazione_linear_controller(input)
     yd_ddot=input(3);
     y=input(4);
     y_dot=input(5);
-    fprintf("SUCAAAAAAAAAAAAAAAAAAAAAA yd %f", yd)
-    fprintf("SUCAAAAAAAAAAAAAAAAAAAAAA yd_dot %f", yd_dot)
-    fprintf("SUCAAAAAAAAAAAAAAAAAAAAAA yd_ddot %f", yd_ddot)
-    fprintf("SUCAAAAAAAAAAAAAAAAAAAAAA y %f", y)
-    fprintf("SUCAAAAAAAAAAAAAAAAAAAAAA y_dot %f", y_dot)
+    fprintf("STAMPA yd %f", yd)
+    fprintf("STAMPA yd_dot %f", yd_dot)
+    fprintf("STAMPA yd_ddot %f", yd_ddot)
+    fprintf("STAMPA y %f", y)
+    fprintf("STAMPA y_dot %f", y_dot)
 
     Kp_alpha=10;
     Kp_beta=10;
